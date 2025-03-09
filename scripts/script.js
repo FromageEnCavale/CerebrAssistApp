c42fbae991e17fc74844986968fb46e4cc8334b6
FromageEnCavale <julescoubetergues@icloud.com>
2025-03-09 13:12:07 +0100

    Add my own markdown

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-/*
-import { marked } from 'https://cdn.jsdelivr.net/npm/marked@15.0.7/+esm';
+// Marked.js
+/*import { marked } from 'https://cdn.jsdelivr.net/npm/marked@15.0.7/+esm';*/
+
 import dompurify from 'https://cdn.jsdelivr.net/npm/dompurify@3.2.4/+esm';
-*/
 
 const deleteButton = document.getElementById("deleteButton");
 
@@ -17,15 +17,20 @@ let conversationHistory = [];
 
 let maxCharacters = 8000;
 
-/*
 function parseMarkdown(text) {
 
-    const rawHTML = marked(text);
+    // Marked.js
+    /*const rawHTML = marked(text);*/
+
+    // New
+    const paragraphs = text.split(/\n\s*\n/);
+
+    // New
+    const rawHTML = paragraphs.map(paragraph => `<p>${paragraph}</p>`).join('');
 
     return dompurify.sanitize(rawHTML, { FORBID_ATTR: ['style'] });
 
 }
-*/
 
 function calculateHistorySize(history) {
 
@@ -111,12 +116,10 @@ async function sendMessage() {
 
             responseText += chunk;
 
-            /*
             responseElement.innerHTML = parseMarkdown(responseText);
-            */
 
-            // New
-            responseElement.textContent = responseText;
+            // Without anythings, raw text
+            /*responseElement.textContent = responseText;*/
 
         }
 
